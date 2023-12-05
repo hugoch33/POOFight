50288c0be74b8642a4a0e1d1155f9dd9bbd8123e
chhugo <hugo.chamand@ynov.com>
2023-12-05 10:04:38 +0100

    adding c++

--- FIght.hpp
+++ FIght.hpp
@@ -1,2 +1,41 @@
-#ifndef 
-#define 
+#ifndef FIGHT
+#define FIGHT
+
+#include <iostream>
+using namespace std;
+
+class Personnage {
+private:
+	string m_nom;
+	int m_pv;
+    int m_stamina;
+    int m_def;
+public:
+	Personnage(string nom, int pv, int stamina, int def) ;
+	~Personnage();
+
+	int get_pv ();
+	string get_nom() const;
+    int get_stamina ();
+
+};
+
+#endif
+
+
+class Vilain {
+private:
+	string m_nom;
+	int m_pv;
+    int m_stamina;
+    int m_def;
+
+public:
+	Personnage(string nom, int pv, int stamina, int def) ;
+	~Personnage();
+
+	int get_pv ();
+	string get_nom() const;
+    int get_stamina ();
+
+};
---
+++ main.cpp
@@ -0,0 +1,3 @@
+#include "Fight.hpp"
+
+#include 
